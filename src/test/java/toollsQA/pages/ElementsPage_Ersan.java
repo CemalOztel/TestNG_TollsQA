3a1c936fb85b140fe39b96fe784ea362475bce00
ersanefe <68191709+ersanefe@users.noreply.github.com>
2023-04-19 13:57:17 +0200

    Ersan created

--- src/test/java/toollsQA/pages/ElementsPage_Ersan.java
+++ src/test/java/toollsQA/pages/ElementsPage_Ersan.java
@@ -1,5 +1,52 @@
 package toollsQA.pages;
 
-public class ElementsPage_Ersan extends  BasePage{
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
 
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+import toollsQA.utilities.BrowserUtils;
+import toollsQA.utilities.Driver;
+
+public class ElementsPage_Ersan extends BasePage {
+
+    DashBoardPage dashBoardPage;
+
+    @FindBy(id = "userName")
+    public WebElement FullName;
+
+    @FindBy(id = "userEmail")
+    public WebElement Email;
+
+    @FindBy(id = "currentAddress")
+    public WebElement currentAdress;
+
+    @FindBy(id = "permanentAddress")
+    public WebElement permanentAddress;
+
+    @FindBy(id = "submit")
+    public WebElement submitBtn;
+
+    @FindBy(className = "rct-icon rct-icon-expand-all")
+    public WebElement expendAllBtn;
+
+    @FindBy(id = "yesRadio")
+    public WebElement yesRadioBtn;
+
+    @FindBy(id = "impressiveRadio")
+    public WebElement impressiveRadioBtn;
+
+    @FindBy(id = "noRadio")
+    public WebElement noRadioBtn;
+
+    @FindBy(xpath = "//span[@class='text-success']")
+    public WebElement selectedRadiocheck;
+
+
+    public void ElementsAssert(String SubTitle) {
+
+        WebElement element = Driver.get().findElement(By.xpath("//div[text()='" + SubTitle + "']"));
+        System.out.println(element.getText());
+        Assert.assertEquals(element.getText(), SubTitle, "Verify that Elements subpage is " + SubTitle);
+    }
 }
