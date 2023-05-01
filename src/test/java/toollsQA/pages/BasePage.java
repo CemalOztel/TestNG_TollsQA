677b7e53e3e6422d9b8fc1851f295a37de02c8db
Cemal <121253924+CemalOztel@users.noreply.github.com>
2023-05-01 20:49:38 +0200

    Update BasePage.java

--- src/test/java/toollsQA/pages/BasePage.java
+++ src/test/java/toollsQA/pages/BasePage.java
@@ -1,1 +1,13 @@
+package toollsQA.pages;
 
+import org.openqa.selenium.support.PageFactory;
+import toollsQA.utilities.Driver;
+
+
+public abstract class BasePage {
+
+public BasePage() {
+            PageFactory.initElements(Driver.get(),this);
+        }
+
+}
